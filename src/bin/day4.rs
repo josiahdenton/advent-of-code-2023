0546fffd9072b6e1bc4c050b77fe361c56b68f38
Josiah Denton <jfranklindenton@gmail.com>
2023-12-07 11:13:57 -0500

    add day 4 part 2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,19 +1,76 @@
-use std::fs;
+use std::{cell::RefCell, collections::HashMap, fs};
 
 use anyhow::Result;
 
 const BASE: u32 = 2;
+
+type Range = (u32, u32);
 
 fn main() -> Result<()> {
     let problem = aoc::fetch_problem()?;
     let cards = fs::read_to_string(&problem.path)?;
 
     println!("sum is: {:?}", find_winning_total(&cards));
+    println!("total scratchers {:?}", find_total_card_count(&cards));
 
     Ok(())
 }
 
-// track number of cards 
+fn find_total_card_count(cards: &str) -> u32 {
+    let mut card_copies: RefCell<HashMap<u32, u32>> = RefCell::new(HashMap::new());
+    let card_count = cards
+        .lines()
+        .map(|card| {
+            let scratcher = card.split(":").collect::<Vec<&str>>();
+            let scratcher_id = scratcher
+                .get(0)
+                .expect("no card title found")
+                .split(' ')
+                .last()
+                .expect("no card id found")
+                .parse::<u32>()
+                .expect("failed id parse");
+
+            let scratcher_content = scratcher
+                .get(1)
+                .expect("no num lists in scratcher")
+                .split("|")
+                .collect::<Vec<&str>>();
+
+            let this_card_count = *card_copies.borrow().get(&scratcher_id).unwrap_or(&1);
+            let total_winning_nums = count_total_winning_nums(scratcher_content);
+            if total_winning_nums > 0 {
+                add_scratcher_copies(
+                    card_copies.get_mut(),
+                    // don't add yourself, and end of range is exlusive so + 1
+                    (scratcher_id + 1, scratcher_id + total_winning_nums + 1),
+                    this_card_count,
+                );
+            }
+
+            this_card_count
+        })
+        .sum();
+
+    println!("{:?}", card_copies.borrow());
+
+    card_count
+}
+
+fn add_scratcher_copies(card_copies: &mut HashMap<u32, u32>, range: Range, this_card_count: u32) {
+    println!("with {this_card_count} x scratchers");
+    print!("adding scratcher ids: ");
+    for scratcher_id in (range.0)..(range.1) {
+        print!("{scratcher_id}, ");
+        if card_copies.contains_key(&scratcher_id) {
+            *card_copies.get_mut(&scratcher_id).unwrap() += this_card_count;
+        } else {
+            // there is always 1 scratcher, so we process based off of this_card_count + 1
+            card_copies.insert(scratcher_id, 1 + this_card_count);
+        }
+    }
+    println!("");
+}
 
 fn find_winning_total(cards: &str) -> u32 {
     cards
@@ -26,17 +83,7 @@ fn find_winning_total(cards: &str) -> u32 {
                 .split("|")
                 .collect::<Vec<&str>>();
 
-            let winning_nums = game_card_lists.get(0).unwrap();
-            let winning_nums = parse_scratch_nums(&winning_nums);
-
-            let card_nums = game_card_lists.get(1).unwrap();
-            let card_nums = parse_scratch_nums(&card_nums);
-
-            let total_winning_nums = card_nums
-                .iter()
-                .filter(|num| winning_nums.contains(num))
-                .count();
-
+            let total_winning_nums = count_total_winning_nums(game_card_lists);
             if total_winning_nums == 0 {
                 return 0;
             }
@@ -46,7 +93,20 @@ fn find_winning_total(cards: &str) -> u32 {
         .sum()
 }
 
-fn parse_scratch_nums(list: &str) -> Vec<u32> {
+fn count_total_winning_nums(card_content: Vec<&str>) -> u32 {
+    let winning_nums = card_content.get(0).unwrap();
+    let winning_nums = to_u32_vec(&winning_nums);
+
+    let card_nums = card_content.get(1).unwrap();
+    let card_nums = to_u32_vec(&card_nums);
+
+    card_nums
+        .iter()
+        .filter(|num| winning_nums.contains(num))
+        .count() as u32
+}
+
+fn to_u32_vec(list: &str) -> Vec<u32> {
     list.split(' ')
         .filter(|s| s.len() > 0)
         .map(|s| s.parse::<u32>().unwrap())
@@ -55,6 +115,7 @@ fn parse_scratch_nums(list: &str) -> Vec<u32> {
 
 #[cfg(test)]
 mod test {
+    use crate::find_total_card_count;
     use crate::find_winning_total;
 
     #[test]
@@ -66,7 +127,9 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 ";
-        let total = find_winning_total(&s);
-        assert!(total == 13, "total not correct: {total}");
+        let winning_sum = find_winning_total(&s);
+        let card_cnt = find_total_card_count(&s);
+        assert!(winning_sum == 13, "total not correct: {winning_sum}");
+        assert!(card_cnt == 30, "card count incorrect: {card_cnt}");
     }
 }
